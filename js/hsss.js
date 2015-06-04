67fc651f66a8eba5007ecd5115587189da901dc4
Celso <celso.white13@gmail.com>
2015-06-04 08:22:44 -0400

    Add audio functionality via wavesurfer.js

--- js/hsss.js
+++ js/hsss.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 	function onScrollInit( items, trigger ) {
 		items.each( function() {
 			var osElement = $(this),
+			osAnimationOffset = osElement.attr('data-os-animation-offset'),
 			osAnimationClassDown = osElement.attr('data-os-animation-down'),
 			osAnimationClassUp = osElement.attr('data-os-animation-up'),
 			osAnimationDelay = osElement.attr('data-os-animation-delay');
@@ -39,6 +40,43 @@ $(document).ready(function() {
 	}
 
 	onScrollInit( $('.os-animation') );
+
+	/*=== Top Label ===*/
+
+	function onScrollInit_toplabel( items, trigger ) {
+		items.each( function() {
+			var osElement = $(this),
+			osAnimationOffset = osElement.attr('data-os-animation-offset'),
+			osAnimationClassDown = osElement.attr('data-os-animation-down'),
+			osAnimationClassUp = osElement.attr('data-os-animation-up'),
+			osAnimationDelay = osElement.attr('data-os-animation-delay');
+			osAnimationDuration = osElement.attr('data-os-animation-duration');
+
+			osElement.css({
+				'-webkit-animation-delay':     osAnimationDelay,
+				'-moz-animation-delay':        osAnimationDelay,
+				'animation-delay':             osAnimationDelay,
+				'-webkit-animation-duration':  osAnimationDuration,
+				'-moz-animation-duration':     osAnimationDuration,
+				'animation-duration':          osAnimationDuration,
+			});
+
+			var osTrigger = ( trigger ) ? trigger : osElement;
+
+			osTrigger.waypoint(function(direction) {
+				if (direction == 'down') {
+					osElement.addClass('animated').addClass(osAnimationClassDown).removeClass(osAnimationClassUp);
+				}
+				if (direction == 'up') {
+					osElement.addClass(osAnimationClassUp).removeClass(osAnimationClassDown);
+				}
+			},{
+				offset: '20%'
+			});
+		});
+	}
+
+	onScrollInit_toplabel( $('.os-label-animation') );
 
 	/*============================
 	HEADER OVERLAY
@@ -105,5 +143,58 @@ $(document).ready(function() {
 	============================*/
 
 	var s = skrollr.init();
+
+	/*============================
+	WAVESURFER
+	============================*/
+
+	/*=== Number Exchange Waveform ===*/
+
+	var number_exchange = Object.create(WaveSurfer);
+
+	number_exchange.init({
+	    container: '#wave',
+	    waveColor: '#4487c7',
+	    progressColor: '#eb5d4b'
+	});
+
+	/* number_exchange.on('ready', function () {
+	    number_exchange.play();
+	}); */
+
+	number_exchange.load('audio/number_exchange.mp3');
+
+	/*=== Play Function Number Exchange ===*/
+
+	var GLOBAL_ACTIONS = {
+	    'play_number_exchange': function () {
+	        number_exchange.playPause();
+	    },
+	};
+
+	/*=== Data Action Query Selector ===*/
+
+	[].forEach.call(document.querySelectorAll('[data-action]'), function (el) {
+        el.addEventListener('click', function (e) {
+            var action = e.currentTarget.dataset.action;
+            if (action in GLOBAL_ACTIONS) {
+                e.preventDefault();
+                GLOBAL_ACTIONS[action](e);
+            }
+        });
+    });
+
+	/*=== Toggle Play/Pause buttons ===*/
+
+    var playButton = document.querySelector('#play');
+    var pauseButton = document.querySelector('#pause');
+    number_exchange.on('play', function () {
+        playButton.style.display = 'none';
+        pauseButton.style.display = '';
+    });
+    number_exchange.on('pause', function () {
+        playButton.style.display = '';
+        pauseButton.style.display = 'none';
+    });
 
 });
